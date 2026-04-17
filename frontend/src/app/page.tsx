85274989a8f2286df175a157734c3b25bf69c491
LionelSocrate <lionelsocrate7@gmail.com>
2026-04-17 12:56:15 +0300

    Initialisation

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/pieces/button";
+
+export default function Home() {
+  return (
+    <div className=" text-2xl ">
+      hello socrate
+      <Button variant="ghost">Click me</Button>
+    </div>
+  );
+}
