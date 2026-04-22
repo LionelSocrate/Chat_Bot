d0da79ac957773ac25eab76eab56e145e4e63cea
VELONTSARA <bernadovelontsara@gmail.com>
2026-04-22 03:09:34 +0300

    correction du bug prisma

--- backend/src/client/client.service.ts
+++ backend/src/client/client.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from "../prisma/prisma.service";
+import { CreateClientDto } from './client-dto/client-user.dto';
+
 
 @Injectable()
-export class ClientService {}
+export class ClientService {
+    constructor(private prisma: PrismaService) {}
+    async getAllClients() {
+        return this.prisma.client.findMany();
+    }
+    async createClient(dto: CreateClientDto) {
+        return this.prisma.client.create({
+            data: {
+                email: dto.email,
+                nom: dto.nom,
+                adresse: dto.adresse,
+                tel: dto.tel,
+            },
+        });
+    }
+    
+}
+
--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -1,11 +1,25 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from "@nestjs/common";
 import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  constructor() {
+    // 1. Créer le pool de connexion avec le driver natif 'pg'
+    const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+    
+    // 2. Créer l'adaptateur Prisma
+    const adapter = new PrismaPg(pool);
+
+    // 3. Passer l'adaptateur au constructeur
+    super({ adapter });
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
+
   async onModuleDestroy() {
     await this.$disconnect();
   }
